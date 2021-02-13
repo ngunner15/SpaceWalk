113364088ff5365d51a74eee880a4df0f3562f14
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-12 22:25:38 -0500

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,11 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Home from './components/Home';
 import Earth from './components/Earth';
 import Mars from './components/Mars';
@@ -6,11 +14,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Earth />
-      <Mars />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/earth">Earth</Link>
+          <Link to="/mars">Mars</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/earth">
+            <Earth />
+          </Route>
+          <Route path="/mars">
+            <Mars />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

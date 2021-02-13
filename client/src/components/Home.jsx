66a5a58efab7b5ed0b8db3e8dd8297ab3aa42a8a
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-13 14:28:40 -0500

    Create a 3d square using react-three-fiber and @react-three/drei libraries

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,25 @@
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+
+function Box() {
+  return(
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
+
 export default function Home(props) {
 
-  return <h1>I am Homepage</h1>
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Box />
+    </Canvas>   
+  )
 }

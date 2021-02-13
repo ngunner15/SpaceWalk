9040bee5f0d9e4ea98d5d5b7fc896af767e68b02
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-13 12:57:47 -0500

    Add get route for Mars API

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,22 @@ app.use('/users', usersRouter);
 
 module.exports = app;
 
-const api = {
+const apiEarth = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 app.get('/getEarthWeather/:city', (req, res) => {
   // console.log(req.params);
   
-  axios.get(`${api.base}weather?q=${req.params.city}&units=metric&APPID=${process.env.EARTH_KEY}`)
+  axios.get(`${apiEarth.base}weather?q=${req.params.city}&units=metric&APPID=${process.env.EARTH_KEY}`)
+        .then(result => {
+          res.json(result.data);
+        });
+})
+
+app.get('/getMarsWeather', (req, res) => {
+  
+  axios.get(`https://api.nasa.gov/insight_weather/?api_key=${process.env.MARS_KEY}&feedtype=json&ver=1.0`)
         .then(result => {
           res.json(result.data);
         });

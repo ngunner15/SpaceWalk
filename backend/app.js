0a700867fa55926d04d83f253276f227a583f844
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-12 19:22:46 -0500

    Create get request for axios.get of earth weather api

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const axios = require('axios');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,3 +20,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+const api = {
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+app.get('/getEarthWeather/:city', (req, res) => {
+  // console.log(req.params);
+  
+  axios.get(`${api.base}weather?q=${req.params.city}&units=metric&APPID=${process.env.EARTH_KEY}`)
+        .then(result => {
+          res.json(result.data);
+        });
+})

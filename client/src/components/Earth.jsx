2670187df1e0e9407db9ae817fb23a8b3f1d3671
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-12 19:25:56 -0500

    Send city to getEarthWeather route

--- client/src/components/Earth.jsx
+++ client/src/components/Earth.jsx
@@ -1,66 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import dateBuilder from '../helpers/DateHelper'
 
-const api = {
-  key: "43197e8edd946d801bfee7217d527143",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
 export default function Earth(props) {
-  const [query, setQuery] = useState('');
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
 
   const search = evt => {
     if (evt.key === "Enter") {
-      axios.get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      axios.get(`/getEarthWeather/${city}`)
         .then(result => {
+          console.log(result);
           setWeather(result.data);
-          setQuery('');
-          console.log(result.data);
+          setCity('');
+          // console.log(result.data);
         });
     }
   }
 
-const dateBuilder = (d) => {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return`${day} ${date} ${month} ${year}`
-}
-
   return (
-  <main>
-    <h1>I am Earth</h1>
-    <div className="search-box">
-      <input
-        type="text"
-        className="search-bar"
-        placeholder="Search..."
-        onChange={e => setQuery(e.target.value)}
-        value={query}
-        onKeyPress={search}
-      />
-    </div>
-  {(typeof weather.main != "undefined") ? (
-    <div>
-      <div className="location-box">
-        <div className="location">{weather.name}, {weather.sys.country}</div>
-        <div className="date">{dateBuilder(new Date())}</div>
+    <main>
+      <h1>I am Earth</h1>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={e => setCity(e.target.value)}
+          value={city}
+          onKeyPress={search}
+        />
       </div>
+      {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date)}</div>
+          </div>
 
-      <div className="weather-box">
-        <div className="temp">
-          {Math.round(weather.main.temp)}°C
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
         </div>
-        <div className="weather">{weather.weather[0].main}</div>
-      </div>
-    </div>
-  ) : ('')}
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+      ) : ('')}
 
-  </main>
+    </main>
   )
 }

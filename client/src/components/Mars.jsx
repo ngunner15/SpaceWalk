9d6ba2a6388a01a896f0dba714d09ddcd21ca025
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-13 12:56:58 -0500

    Get data from NASA's insight API through backend

--- client/src/components/Mars.jsx
+++ client/src/components/Mars.jsx
@@ -1,4 +1,49 @@
+import axios from 'axios';
+
 export default function Mars(props) {
 
-  return <h1>I am Mars</h1>
+  function getWeather() {
+    axios.get(`/getMarsWeather`)
+      .then(result => {
+        const {
+          sol_keys,
+          validity_checks,
+          ...solData
+        } = result.data
+        console.log(solData);
+      })
+    //           const temp = Object.entries(solData).map(([sol, data]) => {
+    //             return {
+    //               sol: sol,
+    //               // maxTemp: data.AT.mx,
+    //               // minTemp: data.AT.mn,
+    //               // windSpeed: data.HWS.av,
+    //               // windDirectionDegrees: data.WD.most_common.compass_degrees,
+    //               // windDirectionCardinal: data.WD.most_common.compass_point,
+    //               date: new Date(data.First_UTC)
+    //             }
+    //           })
+  }
+
+  getWeather();
+  return (
+    <main>
+      <h1>I am Mars</h1>
+      {/* <div>
+        <div className="location-box">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
+          <div className="date">{dateBuilder(new Date)}</div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">
+            {Math.round(weather.main.temp)}°C
+          </div>
+          <div className="weather">
+            {weather.weather[0].main}
+          </div>
+        </div>
+      </div> */}
+    </main>
+  )
 }

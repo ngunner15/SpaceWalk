0b6356df1d283b3f549bcf76285dd59998a72852
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-15 15:57:52 -0500

    Add a working 3d planet

--- client/src/components/Venus.jsx
+++ client/src/components/Venus.jsx
@@ -1,41 +1,37 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "react-three-fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
-// import { Sphere } from "./Sphere";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function Box() {
-  return(
-    <mesh>
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  )
-}
-
-// function Sphere() {
-//   return (
-//     <mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>
-//       <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-//       <meshStandardMaterial
-//         attach="material"
-//         color="white"
-//         transparent
-//         roughness={0.1}
-//         metalness={0.1}
-//       />
+// function Box() {
+//   return(
+//     <mesh>
+//       <boxBufferGeometry attach="geometry" />
+//       <meshLambertMaterial attach="material" color="hotpink" />
 //     </mesh>
-//   );
+//   )
 // }
+
+const Planet = () => {
+  const [model, setModel] =useState();
+  useEffect(() => {
+    new GLTFLoader().load('models/venus/scene.gltf', setModel);
+  }, [])
+  console.log(model);
+  return model ? <primitive object={model.scene} /> : null;
+}
 
 export default function Venus(props) {
 
   return (
     <Canvas>
-      <OrbitControls />
+      <OrbitControls autoRotate />
       <directionalLight intensity={1} />
       <ambientLight intensity={0.5} />
-        <Box />
-        <Stars />
-    </Canvas>   
+      <Suspense>
+        <Planet />
+      </Suspense>
+      <Stars />
+    </Canvas>
   )
 }

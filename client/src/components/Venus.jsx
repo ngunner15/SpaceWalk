a90f67c9321173ce9739797d30c2f706829fe85d
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-15 16:35:46 -0500

    Import OrbitControl from three.js instead of drei, add autoRotate, minDistance, maxDistance properties to orbitControls component

--- client/src/components/Venus.jsx
+++ client/src/components/Venus.jsx
@@ -1,7 +1,11 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "react-three-fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame, useThree, extend } from "react-three-fiber";
+import {  Stars } from "@react-three/drei";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// Extend will make OrbitControls available as a JSX element called orbitControls for us to use.
+extend({ OrbitControls });
 
 // function Box() {
 //   return(
@@ -11,6 +15,30 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 //     </mesh>
 //   )
 // }
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls component.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={true}
+      minDistance={165}
+      maxDistance={275}
+      autoRotate
+      autoRotateSpeed={0.8}
+    />
+  );
+};
 
 const Planet = () => {
   const [model, setModel] =useState();
@@ -25,7 +53,7 @@ export default function Venus(props) {
 
   return (
     <Canvas>
-      <OrbitControls autoRotate />
+      <CameraControls />
       <directionalLight intensity={1} />
       <ambientLight intensity={0.5} />
       <Suspense>

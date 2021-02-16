f917bd6041b42b14b4cc1a311bbf461ae1ddcb3b
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-16 14:26:50 -0500

    Use CameraControls and RenderPlanet components to show 3D model and pass planet name as prop

--- client/src/components/Mercury.jsx
+++ client/src/components/Mercury.jsx
@@ -1,40 +1,8 @@
-import React, { Suspense, useEffect, useState, useRef } from "react";
-import { Canvas, useFrame, useThree, extend } from "react-three-fiber";
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
 import {  Stars } from "@react-three/drei";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-extend({ OrbitControls });
-
-const CameraControls = () => {
-  const {
-    camera,
-    gl: { domElement },
-  } = useThree();
-
-  const controls = useRef();
-  useFrame((state) => controls.current.update());
-  return (
-    <orbitControls
-      ref={controls}
-      args={[camera, domElement]}
-      enableZoom={true}
-      minDistance={165}
-      maxDistance={275}
-      autoRotate
-      autoRotateSpeed={0.8}
-    />
-  );
-};
-
-const Planet = () => {
-  const [model, setModel] =useState();
-  useEffect(() => {
-    new GLTFLoader().load('models/mercury/scene.gltf', setModel);
-  }, [])
-  console.log(model);
-  return model ? <primitive object={model.scene} /> : null;
-}
+import CameraControls from './CameraControls';
+import RenderPlanet from './RenderPlanet'
 
 export default function Mercury(props) {
 
@@ -44,7 +12,7 @@ export default function Mercury(props) {
       {/* <directionalLight intensity={0.5} /> */}
       <ambientLight intensity={0.6} />
       <Suspense>
-        <Planet />
+        <RenderPlanet planets="mercury"/>
       </Suspense>
       <Stars 
         radius={150} // Radius of the inner sphere (default=100)

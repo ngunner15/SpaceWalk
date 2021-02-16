af20d699bbee6ff4f798e41c6a4a92b09144bd15
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-16 17:40:23 -0500

    Add 3D model

--- client/src/components/Mars.jsx
+++ client/src/components/Mars.jsx
@@ -1,3 +1,8 @@
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import {  Stars } from "@react-three/drei";
+import CameraControls from './CameraControls';
+import RenderPlanet from './RenderPlanet'
 import axios from 'axios';
 
 export default function Mars(props) {
@@ -28,22 +33,41 @@ export default function Mars(props) {
   getWeather();
   return (
     <main>
+      <div className="planet-details">
       <h1>I am Mars</h1>
       {/* <div>
-        <div className="location-box">
+        <div className="location-data">
           <div className="location">{weather.name}, {weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date)}</div>
         </div>
 
-        <div className="weather-box">
-          <div className="temp">
-            {Math.round(weather.main.temp)}°C
+        <div className="weather-data">
+          <div className="min-temperature">
+            Min temp: {Math.round(weather.main.temp_min)}°C
           </div>
-          <div className="weather">
+          <div className="max-temperature">
+            Max temp: {Math.round(weather.main.temp_max)}°C
+          </div>
+          <div className="weather-type">
             {weather.weather[0].main}
           </div>
         </div>
       </div> */}
+      </div>
+      <Canvas className="planet-model">
+        <CameraControls zoomedInDistance={170} zoomedOutDistance={280} />
+        {/* <directionalLight intensity={0.5} /> */}
+        <ambientLight intensity={0.6} />
+        <Suspense>
+          <RenderPlanet planets="mars"/>
+        </Suspense>
+        <Stars 
+          radius={150} // Radius of the inner sphere (default=100)
+          depth={70} // Depth of area where stars should fit (default=50)
+          count={4000} // Amount of stars (default=5000)
+          factor={5} // Size factor (default=4)
+        />
+      </Canvas>
     </main>
   )
 }

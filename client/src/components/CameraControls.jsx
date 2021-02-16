762b806c50fb1719f3d2c168088ab36e9d8e7795
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-16 14:50:21 -0500

    Made components which can be used in other planets

---
+++ client/src/components/CameraControls.jsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+import { useFrame, useThree, extend } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls });
+
+export default function CameraControls(props) {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={true}
+      minDistance={165}
+      maxDistance={275}
+      autoRotate
+      autoRotateSpeed={0.8}
+    />
+  );
+};

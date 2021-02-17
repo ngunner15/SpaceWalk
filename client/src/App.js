06f09cc9abf02aab2a974bdb31be5cdf1d713b61
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-17 13:10:38 -0500

    import Apod from components

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,19 @@ import {
 } from "react-router-dom";
 
 import Home from './components/Home';
-import Earth from './components/Earth';
-import Mars from './components/Mars';
-import Venus from './components/Venus';
-import Mercury from "./components/Mercury";
 import Login from "./components/Login";
 import Admin from "./components/Admin";
+import Mercury from "./components/Mercury";
+import Venus from './components/Venus';
+import Earth from './components/Earth';
+import Mars from './components/Mars';
 import Logout from "./components/Logout";
 import Jupiter from "./components/Jupiter";
 import Saturn from "./components/Saturn";
 import Uranus from "./components/Uranus";
 import Neptune from "./components/Neptune";
 import Pluto from "./components/Pluto";
+import Apod from './components/Apod';
 
 import './App.css';
 

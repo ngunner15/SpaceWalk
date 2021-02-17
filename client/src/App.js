a07365994d98512ef89a85f283757662e78631e4
Steven Chen <schenn1992@gmail.com>
2021-02-17 15:01:45 -0500

    transfer code from incorrect folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,78 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import Earth from './components/Earth';
 import Mars from './components/Mars';
 import Venus from './components/Venus';
-import Mercury from "./components/Mercury";
-import Login from "./components/Login";
-import Admin from "./components/Admin";
-import Logout from "./components/Logout";
-import Jupiter from "./components/Jupiter";
-import Saturn from "./components/Saturn";
-import Uranus from "./components/Uranus";
-import Neptune from "./components/Neptune";
-import Pluto from "./components/Pluto";
+import Mercury from './components/Mercury';
+import Login from './components/Login';
+import Admin from './components/Admin';
+import Logout from './components/Logout';
+import Jupiter from './components/Jupiter';
+import Saturn from './components/Saturn';
+import Uranus from './components/Uranus';
+import Neptune from './components/Neptune';
+import Pluto from './components/Pluto';
 
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/mercury">Mercury</Link>
-          <Link to="/earth">Earth</Link>
-          <Link to="/venus">Venus</Link>
-          <Link to="/mars">Mars</Link>
-          <Link to="/jupiter">Jupiter</Link>
-          <Link to="/saturn">Saturn</Link>
-          <Link to="/uranus">Uranus</Link>
-          <Link to="/neptune">Neptune</Link>
-          <Link to="/pluto">Pluto</Link>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/mercury'>Mercury</Link>
+          <Link to='/earth'>Earth</Link>
+          <Link to='/venus'>Venus</Link>
+          <Link to='/mars'>Mars</Link>
+          <Link to='/jupiter'>Jupiter</Link>
+          <Link to='/saturn'>Saturn</Link>
+          <Link to='/uranus'>Uranus</Link>
+          <Link to='/neptune'>Neptune</Link>
+          <Link to='/pluto'>Pluto</Link>
         </nav>
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact component={Home}>
             <Home />
           </Route>
-          <Route path="/mercury">
+          <Route path='/mercury'>
             <Mercury />
           </Route>
-          <Route path="/venus">
+          <Route path='/venus'>
             <Venus />
           </Route>
-          <Route path="/earth">
+          <Route path='/earth'>
             <Earth />
           </Route>
-          <Route path="/mars">
+          <Route path='/mars'>
             <Mars />
           </Route>
-          <Route path="/jupiter">
+          <Route path='/jupiter'>
             <Jupiter />
           </Route>
-          <Route path="/saturn">
+          <Route path='/saturn'>
             <Saturn />
           </Route>
-          <Route path="/uranus">
+          <Route path='/uranus'>
             <Uranus />
           </Route>
-          <Route path="/neptune">
+          <Route path='/neptune'>
             <Neptune />
           </Route>
-          <Route path="/pluto">
+          <Route path='/pluto'>
             <Pluto />
           </Route>
-          <Route path="/login"><Login /></Route>
-          <Route path="/admin"> <Admin /></Route>
-          <Route path = "/logout">
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/admin'>
+            {' '}
+            <Admin />
+          </Route>
+          <Route path='/logout'>
             <Logout />
           </Route>
         </Switch>

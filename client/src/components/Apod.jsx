2d2bdad9059a4806f64d1ea0d09a9d062b5d70bb
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-17 16:05:40 -0500

    Add date field which shows picture of that date

--- client/src/components/Apod.jsx
+++ client/src/components/Apod.jsx
@@ -3,21 +3,40 @@ import axios from 'axios';
 
 export default function Apod(props) {
   const [data, setData] = useState({});
+  const [date, setDate] = useState("");
   const api = 'IkM9PBhHgGHbzBvRtKGk8gOaPaLGZZ0qVSeZH0eJ';
 
-  const search = async () => {
-    let res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api}`);
-
-    setData(res.data);
+  const search = async (evt) => {
+    if (date === "") {
+      let res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api}`);
+      setData(res.data);
+    } else if (evt.key === "Enter") {
+      let res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api}&date=${date}`)
+      .then(setDate(""));
+      setData(res.data);
+    }
   }
 
   return (
     <div>
       <h1>I am APOD</h1>
-      <button onClick={search}>Show Picture</button>
+      <label for="date">Select date:</label>
+      <input
+        type="date"
+        id="date"
+        min="2010-01-01"
+        max="2021-02-17"
+        value={date}
+        onChange={e => setDate(e.target.value)}
+        onKeyPress={search}
+      />
+      <button onClick={search}>Show Today's Picture</button>
       {(data.media_type === "video") ? (
         <div>
-          URL link: {data.url}
+          {/* URL link: {data.url} */}
+          <video width="750" height="500" controls >
+          <source src={data.url} type="video/mp4"/>
+          </video>
         </div>
       ) : (
         <div>

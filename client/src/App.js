5c87e8494bb6995390f1a802c93187ff3b0da111
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-18 18:50:10 -0500

    Comment out routes for every planet

--- client/src/App.js
+++ client/src/App.js
@@ -22,73 +22,76 @@ import './App.css';
 function App() {
 
   return (
-    <Router>
-      <div className='App'>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/mercury">Mercury</Link>
-          <Link to="/venus">Venus</Link>
-          <Link to="/earth">Earth</Link>
-          <Link to="/mars">Mars</Link>
-          <Link to="/jupiter">Jupiter</Link>
-          <Link to="/saturn">Saturn</Link>
-          <Link to="/uranus">Uranus</Link>
-          <Link to="/neptune">Neptune</Link>
-          <Link to="/pluto">Pluto</Link>
-          <Link to="/apod">APOD</Link>
-          <Link to="/photos">Photos</Link>
-        </nav>
-        <Switch>
-          <Route path='/' exact component={Home}>
-            <Home />
-          </Route>
-          <Route path='/mercury'>
-            <Mercury />
-          </Route>
-          <Route path='/venus'>
-            <Venus />
-          </Route>
-          <Route path='/earth'>
-            <Earth />
-          </Route>
-          <Route path='/mars'>
-            <Mars />
-          </Route>
-          <Route path='/jupiter'>
-            <Jupiter />
-          </Route>
-          <Route path='/saturn'>
-            <Saturn />
-          </Route>
-          <Route path='/uranus'>
-            <Uranus />
-          </Route>
-          <Route path='/neptune'>
-            <Neptune />
-          </Route>
-          <Route path='/pluto'>
-            <Pluto />
-          </Route>
-          <Route path="/apod">
-            <Apod />
-          </Route>
-          <Route path="/photos">
-            <PhotoGallery />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/admin'>
-            {' '}
-            <Admin />
-          </Route>
-          <Route path='/logout'>
-            <Logout />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      <Home />
+    </div>
+    // <Router>
+    //   <div className='App'>
+    //     <nav>
+    //       <Link to="/">Home</Link>
+    //       <Link to="/login">Login</Link>
+    //       <Link to="/mercury">Mercury</Link>
+    //       <Link to="/venus">Venus</Link>
+    //       <Link to="/earth">Earth</Link>
+    //       <Link to="/mars">Mars</Link>
+    //       <Link to="/jupiter">Jupiter</Link>
+    //       <Link to="/saturn">Saturn</Link>
+    //       <Link to="/uranus">Uranus</Link>
+    //       <Link to="/neptune">Neptune</Link>
+    //       <Link to="/pluto">Pluto</Link>
+    //       <Link to="/apod">APOD</Link>
+    //       <Link to="/photos">Photos</Link>
+    //     </nav>
+    //     <Switch>
+    //       <Route path='/' exact component={Home}>
+    //         <Home />
+    //       </Route>
+    //       <Route path='/mercury'>
+    //         <Mercury />
+    //       </Route>
+    //       <Route path='/venus'>
+    //         <Venus />
+    //       </Route>
+    //       <Route path='/earth'>
+    //         <Earth />
+    //       </Route>
+    //       <Route path='/mars'>
+    //         <Mars />
+    //       </Route>
+    //       <Route path='/jupiter'>
+    //         <Jupiter />
+    //       </Route>
+    //       <Route path='/saturn'>
+    //         <Saturn />
+    //       </Route>
+    //       <Route path='/uranus'>
+    //         <Uranus />
+    //       </Route>
+    //       <Route path='/neptune'>
+    //         <Neptune />
+    //       </Route>
+    //       <Route path='/pluto'>
+    //         <Pluto />
+    //       </Route>
+    //       <Route path="/apod">
+    //         <Apod />
+    //       </Route>
+    //       <Route path="/photos">
+    //         <PhotoGallery />
+    //       </Route>
+    //       <Route path='/login'>
+    //         <Login />
+    //       </Route>
+    //       <Route path='/admin'>
+    //         {' '}
+    //         <Admin />
+    //       </Route>
+    //       <Route path='/logout'>
+    //         <Logout />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 

bae73dfb1d4968e64de6af7ee8df19ef03b7a679
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-19 15:46:00 -0500

    Moved navbar to another component

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,15 @@ import Home from './components/Home';
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Admin from "./components/Admin";
-import Mercury from "./components/Mercury";
-import Venus from './components/Venus';
-import Earth from './components/Earth';
-import Mars from './components/Mars';
-import Jupiter from "./components/Jupiter";
-import Saturn from "./components/Saturn";
-import Uranus from "./components/Uranus";
-import Neptune from "./components/Neptune";
-import Pluto from "./components/Pluto";
+import Mercury from "./components/planets/Mercury";
+import Venus from './components/planets/Venus';
+import Earth from './components/planets/Earth';
+import Mars from './components/planets/Mars';
+import Jupiter from "./components/planets/Jupiter";
+import Saturn from "./components/planets/Saturn";
+import Uranus from "./components/planets/Uranus";
+import Neptune from "./components/planets/Neptune";
+import Pluto from "./components/planets/Pluto";
 import Apod from './components/Apod';
 import PhotoGallery from './components/PhotoGallery';
 import Favourites from './components/Favourites';
@@ -24,75 +24,60 @@ function App() {
 
   return (
     <div className='App'>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path='/' >
+            <Home />
+          </Route>
+          <Route path='/mercury'>
+            <Mercury />
+          </Route>
+          <Route path='/venus'>
+            <Venus />
+          </Route>
+          <Route path='/earth'>
+            <Earth />
+          </Route>
+          <Route path='/mars'>
+            <Mars />
+          </Route>
+          <Route path='/jupiter'>
+            <Jupiter />
+          </Route>
+          <Route path='/saturn'>
+            <Saturn />
+          </Route>
+          <Route path='/uranus'>
+            <Uranus />
+          </Route>
+          <Route path='/neptune'>
+            <Neptune />
+          </Route>
+          <Route path='/pluto'>
+            <Pluto />
+          </Route>
+          <Route path="/apod">
+            <Apod />
+          </Route>
+          <Route path="/photos">
+            <PhotoGallery />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/admin'>
+            {' '}
+            <Admin />
+          </Route>
+          <Route path='/logout'>
+            <Logout />
+          </Route>
+          <Route path='/favourites'>
+            <Favourites />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-    // <Router>
-    //   <div className='App'>
-    //     <nav>
-    //       <Link to="/">Home</Link>
-    //       <Link to="/login">Login</Link>
-    //       <Link to="/mercury">Mercury</Link>
-    //       <Link to="/venus">Venus</Link>
-    //       <Link to="/earth">Earth</Link>
-    //       <Link to="/mars">Mars</Link>
-    //       <Link to="/jupiter">Jupiter</Link>
-    //       <Link to="/saturn">Saturn</Link>
-    //       <Link to="/uranus">Uranus</Link>
-    //       <Link to="/neptune">Neptune</Link>
-    //       <Link to="/pluto">Pluto</Link>
-    //       <Link to="/apod">APOD</Link>
-    //       <Link to="/photos">Photos</Link>
-    //     </nav>
-    //     <Switch>
-    //       <Route path='/' exact component={Home}>
-    //         <Home />
-    //       </Route>
-    //       <Route path='/mercury'>
-    //         <Mercury />
-    //       </Route>
-    //       <Route path='/venus'>
-    //         <Venus />
-    //       </Route>
-    //       <Route path='/earth'>
-    //         <Earth />
-    //       </Route>
-    //       <Route path='/mars'>
-    //         <Mars />
-    //       </Route>
-    //       <Route path='/jupiter'>
-    //         <Jupiter />
-    //       </Route>
-    //       <Route path='/saturn'>
-    //         <Saturn />
-    //       </Route>
-    //       <Route path='/uranus'>
-    //         <Uranus />
-    //       </Route>
-    //       <Route path='/neptune'>
-    //         <Neptune />
-    //       </Route>
-    //       <Route path='/pluto'>
-    //         <Pluto />
-    //       </Route>
-    //       <Route path="/apod">
-    //         <Apod />
-    //       </Route>
-    //       <Route path="/photos">
-    //         <PhotoGallery />
-    //       </Route>
-    //       <Route path='/login'>
-    //         <Login />
-    //       </Route>
-    //       <Route path='/admin'>
-    //         {' '}
-    //         <Admin />
-    //       </Route>
-    //       <Route path='/logout'>
-    //         <Logout />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
   );
 }
 

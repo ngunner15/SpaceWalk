36c655e1e8ddcd653862d02234ef711a92e5ea98
Steven Chen <schenn1992@gmail.com>
2021-02-19 20:55:50 -0500

    style apod page

--- client/src/components/Apod.jsx
+++ client/src/components/Apod.jsx
@@ -1,42 +1,40 @@
 import React, { useState } from 'react';
+import '../apod.css';
 import axios from 'axios';
 
 export default function Apod(props) {
   const [data, setData] = useState({});
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState('');
 
   const search = async () => {
-    if (date === "") {
-      axios.get(`/getPictureOfDay`)
-      .then(result => {
+    if (date === '') {
+      axios.get(`/getPictureOfDay`).then((result) => {
         // console.log(result);
         setData(result.data);
       });
     } else {
-      axios.get(`/getPictureOfDate/${date}`)
-      .then(result => {
+      axios.get(`/getPictureOfDate/${date}`).then((result) => {
         // console.log(result);
         setData(result.data);
-        setDate("");
+        setDate('');
       });
     }
-  }
+  };
 
   return (
-    <div>
-      <h1>I am APOD</h1>
-      <label htmlFor="date">Select date:</label>
+    <div className='main-page'>
+      <label htmlFor='date'>Select date:</label>
       <input
-        type="date"
-        id="date"
-        min="2010-01-01"
-        max="2021-02-17"
+        type='date'
+        id='date'
+        min='2010-01-01'
+        max='2021-02-17'
         value={date}
-        onChange={e => setDate(e.target.value)}
+        onChange={(e) => setDate(e.target.value)}
         onSelect={search}
       />
       <button onClick={search}>Show Today's Picture</button>
-      {(data.media_type === "video") ? (
+      {data.media_type === 'video' ? (
         <div>
           <p>Today you get to see a video!!!</p>
           <a href={data.url}>Click here</a>
@@ -47,7 +45,6 @@ export default function Apod(props) {
           <img src={data.hdurl}></img>
         </div>
       )}
-      
     </div>
   );
 }

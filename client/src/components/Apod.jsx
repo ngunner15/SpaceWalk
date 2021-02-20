8f63c1dc8b7cde5d1b1bea10728117aa1e86ba55
Steven Chen <schenn1992@gmail.com>
2021-02-20 16:22:04 -0500

    add title and set style

--- client/src/components/Apod.jsx
+++ client/src/components/Apod.jsx
@@ -23,17 +23,22 @@ export default function Apod(props) {
 
   return (
     <div className='main-page'>
-      <label htmlFor='date'>Select date:</label>
-      <input
-        type='date'
-        id='date'
-        min='2010-01-01'
-        max='2021-02-17'
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        onSelect={search}
-      />
-      <button onClick={search}>Show Today's Picture</button>
+      <div className='title'>
+        <h1>🚀 View of the Day 🚀</h1>
+      </div>
+      <div className='container'>
+        <label htmlFor='date'>Select date: </label>
+        <input
+          type='date'
+          id='date'
+          min='2010-01-01'
+          max='2021-02-17'
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          onSelect={search}
+        />
+        <button onClick={search}>Show Today's Picture</button>
+      </div>
       {data.media_type === 'video' ? (
         <div>
           <p>Today you get to see a video!!!</p>

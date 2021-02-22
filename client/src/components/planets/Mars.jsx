b5a724668f6231915eb0c6f3f6fdded364204d20
Steven Chen <schenn1992@gmail.com>
2021-02-22 12:04:07 -0500

    fix minor page error

--- client/src/components/planets/Mars.jsx
+++ client/src/components/planets/Mars.jsx
@@ -31,7 +31,6 @@ export default function Mars(props) {
         <div className='container-right'>
           <h2>Planet Information:</h2>
           <div className='planet-details'>
-            <h1>I am Mars</h1>
             <div>Moons:</div>
             <div>Min Temp: -129°C</div>
             <div>Max Temp: 20°C</div>

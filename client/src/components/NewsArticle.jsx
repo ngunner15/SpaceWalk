3b719ccedbc161dd02482513472d3fefacb840db
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-22 18:48:37 -0500

    Open new tab onClick

--- client/src/components/NewsArticle.jsx
+++ client/src/components/NewsArticle.jsx
@@ -7,7 +7,7 @@ export default function NewsArticle({ data }) {
       <div className="news-content">
       <h1 className="news__title">{data.title}</h1>
       <p className="news__desc">{data.description}</p>
-      <span className="news__url"><a href={data.url}>Read Full Article...</a></span> <br />
+      <span className="news__url"><a href={data.url} target="_blank">Read Full Article...</a></span> <br />
       <span className="news__published">{data.date}</span>
       </div>
       {data.image ? (<img className="news-image" src={data.image} alt="Article Image"></img>) : ("")}

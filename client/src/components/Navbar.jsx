9aee631edbbc482102a4bbf7a4382490c3c42573
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-23 15:30:25 -0500

    Add Dropdown of planets using react-bootstrap

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Nav, NavDropdown, Navbar } from 'react-bootstrap';
 
-export default function Navbar(props) {
+export default function Navigation(props) {
+
   return (
-    <nav>
-      <Link to="/login">Login</Link>
-      <Link to="/mercury">Mercury</Link>
-      <Link to="/venus">Venus</Link>
-      <Link to="/earth">Earth</Link>
-      <Link to="/mars">Mars</Link>
-      <Link to="/jupiter">Jupiter</Link>
-      <Link to="/saturn">Saturn</Link>
-      <Link to="/uranus">Uranus</Link>
-      <Link to="/neptune">Neptune</Link>
-      <Link to="/pluto">Pluto</Link>
-      <Link to="/apod">APOD</Link>
-      <Link to="/photos">Photos</Link>
-      <Link to="/news">News</Link>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>SpaceWalk</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link eventKey="10" href="/login">Login</Nav.Link>
+          <Nav.Link eventKey="11" href="/apod">APOD</Nav.Link>
+          <Nav.Link eventKey="12" href="/photos">Photos</Nav.Link>
+          <Nav.Link eventKey="13" href="/news">News</Nav.Link>
+          <NavDropdown title="Planets" id="basic-nav-dropdown">
+            <NavDropdown.Item eventKey="1" href="/mercury">Mercury</NavDropdown.Item>
+            <NavDropdown.Item eventKey="2" href="/venus">Venus</NavDropdown.Item>
+            <NavDropdown.Item eventKey="3" href="/earth">Earth</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4" href="/mars">Mars</NavDropdown.Item>
+            <NavDropdown.Item eventKey="5" href="/jupiter">Jupiter</NavDropdown.Item>
+            <NavDropdown.Item eventKey="6" href="/saturn">Saturn</NavDropdown.Item>
+            <NavDropdown.Item eventKey="7" href="/uranus">Uranus</NavDropdown.Item>
+            <NavDropdown.Item eventKey="8" href="/neptune">Neptune</NavDropdown.Item>
+            <NavDropdown.Item eventKey="9" href="/pluto">Pluto</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
